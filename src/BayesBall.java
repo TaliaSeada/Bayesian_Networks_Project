e9bf3eb1c3cba0529c0c9d88bcd2ef667d3e0dd0
Talia Seada <talias1235@gmail.com>
2021-11-17 20:48:59 +0200

    fix bayes ball

--- src/BayesBall.java
+++ src/BayesBall.java
@@ -1,60 +1,76 @@
 import java.util.ArrayList;
-import java.util.Stack;
+import java.util.HashMap;
 
 public class BayesBall {
-    /**
-     * @param evidence - givens
-     * @return
-     */
+    private static ArrayList<bayesianNode> passed = new ArrayList<>();
+//    private static HashMap<String,bayesianNode> passed = new HashMap<String,bayesianNode>();
 
     public static String isInd(bayesianNetwork bn, bayesianNode src, bayesianNode dest, ArrayList<bayesianNode> evidence) {
 //        System.out.println(evidence);
-        if (bayesBall(bn, src, dest, evidence,null)) {
+        if (bayesBall(bn, src, dest, evidence, null, passed)) {
             return "yes"; //independent
         } else {
             return "no"; //dependent
         }
     }
+
     //bayes ball: dependent return false, independent return true
-    private static boolean bayesBall(bayesianNetwork bn, bayesianNode src, bayesianNode dest, ArrayList<bayesianNode> evidence, bayesianNode last) {
+    private static boolean bayesBall(bayesianNetwork bn, bayesianNode src, bayesianNode dest, ArrayList<bayesianNode> evidence, bayesianNode last, ArrayList<bayesianNode> passed) {
         if (src.equals(dest)) return false;
         //given
-        if(evidence.contains(src)){
+        if (evidence.contains(src)) {
             //came from child
-            if(src.getChildren().contains(last)){
+            if (src.getChildren().contains(last)) {
                 return true;
             }
             //came from parent
-            else{
-                for(int i=0;i<src.getParents().size();i++) {
-                    if(!bayesBall(bn,src.getParents().get(i),dest,evidence,src))
-                        return false;
+            else {
+                for (int i = 0; i < src.getParents().size(); i++) {
+                    /*
+                        In order to prevent endless loop between the nodes, we will keep the nodes we can't
+                        come back to in a memory.
+                        Only if this is the first time we get to this node - go in, else move on
+                     */
+                    if (!passed.contains(src.getParents().get(i))) {
+                        passed.add(src);
+                        if (!bayesBall(bn, src.getParents().get(i), dest, evidence, src, passed))
+                            return false;
+                    }
                 }
                 return true;
             }
         }
         //not given
-        else{
-            if(src.getParents().contains(last)){
-                for(int i=0;i<src.getChildren().size();i++) {
-                    if(!bayesBall(bn,src.getChildren().get(i),dest,evidence,src))
+        else {
+            //came from parent
+            if (src.getParents().contains(last)) {
+                for (int i = 0; i < src.getChildren().size(); i++) {
+                    if (!bayesBall(bn, src.getChildren().get(i), dest, evidence, src, passed))
                         return false;
+
                 }
                 return true;
             }
-            else{
-                for(int i=0;i<src.getParents().size();i++) {
-                    if(!bayesBall(bn,src.getParents().get(i),dest,evidence,src))
-                        return false;
+            //came from child
+            else {
+                for (int i = 0; i < src.getParents().size(); i++) {
+                    // Only if this is the first time we get to this node - go in, else move on
+                    if (!passed.contains(src.getParents().get(i))) {
+                        passed.add(src);
+                        if (!bayesBall(bn, src.getParents().get(i), dest, evidence, src, passed))
+                            return false;
+                    }
                 }
-                for(int i=0;i<src.getChildren().size();i++) {
-                    if(!bayesBall(bn,src.getChildren().get(i),dest,evidence,src))
-                        return false;
+                for (int i = 0; i < src.getChildren().size(); i++) {
+                        if (!bayesBall(bn, src.getChildren().get(i), dest, evidence, src, passed))
+                            return false;
                 }
                 return true;
 
             }
+
         }
+
     }
 
 }
