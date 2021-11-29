ba90d93a23dd991d8fac3dc147086ce846ed603f
Talia Seada <talias1235@gmail.com>
2021-11-30 00:06:58 +0200

    write to file function

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,53 +1,24 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Ex1 {
 
     public static void main(String[] args) {
-        bayesianNetwork BN = new bayesianNetwork();
-        BN = readXMLfile.read_net("src/alarm_net.xml");
-//        BN = readXMLfile.read_net("src/big_net.xml");
-//        BN = readXMLfile.read_net("src/example.xml");
-
         String path = "src/input2.txt";
         readTextfile reader = new readTextfile(path);
         String s = reader.readfile();
-        System.out.println(s);
-//        System.out.println(BN._bayesianNetwork);
-
-        int node  = 2;
-//        System.out.println(BN._bayesianNetwork.get(node).getCPT());
-//        String[] ev = {"B=T"};
-////        String[] ev = {"C2=v1","C1=T"};
-//        factor f = new factor(BN._bayesianNetwork.get(node).getCPT(), ev);
-//        System.out.println(f.factor.get(0));
-//        f.removeIrrelevantRows();
-//        System.out.println(f.factor);
-
-//        bayesianNode query = BN._bayesianNetwork.get(node);
-//        bayesianNode hidden = BN.returnByName("B0");
-//        boolean an = isAncestor(hidden,query);
-//        System.out.println(an);
-
-//        String q = "A2=T";
-//        String[] h = {"D1", "B3", "C1", "B0", "A1", "B1", "A3"};
-//        String[] e = {"C3=T", "B2=F", "C2=v3"};
-//        variableElimination ve = new variableElimination(q, h, e, BN);
-//        System.out.println(ve.factors);
-//        System.out.println(ve.factors.get(0));
-//        System.out.println(ve.factors.get(4));
-//        factor n = ve.join(ve.factors.get(0), ve.factors.get(4), BN.returnByName("A1"));
-//        System.out.println(n);
-
-        String q = "B=T";
-        String[] h = {"A", "E"};
-        String[] e = {"J=T", "M=T"};
-        variableElimination ve = new variableElimination(q, h, e, BN);
-//        System.out.println(ve.factors);
-
-
-
+//        System.out.println(s);
+        writeToFile(s);
     }
 
-    public void writefile() {
-//        TODO
+    public static void writeToFile(String s) {
+        try {
+            FileWriter myWriter = new FileWriter("output.txt");
+            myWriter.write(s);
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
--- src/readTextfile.java
+++ src/readTextfile.java
@@ -32,50 +32,34 @@ public class readTextfile {
     public String readfile() {
         String ans = "";
         BN = readXMLfile.read_net("src/" + this.lines.get(0));
-//        ArrayList<String> probs = new ArrayList<String>();
-//        ArrayList<String> hiddens = new ArrayList<String>();
         for (int i = 1; i < lines.size(); i++) {
             if (lines.get(i).charAt(0) == 'P') {
                 String[] probs = new String[2];
                 String[] hiddens = new String[2];
                 String[] eliminate_split = lines.get(i).split(" ");
-//                System.out.println(Arrays.toString(eliminate_split));
                 probs[0] = eliminate_split[0];
                 hiddens[0] = eliminate_split[1];
-//                System.out.println(probs[0]);
                 probs[0] = probs[0].replace("P(","");
-//                System.out.println(probs[0]);
                 probs[0] = probs[0].replace(")","");
-//                System.out.println(probs[0]);
-//                System.out.println(hiddens);
                 String[] hidden = hiddens[0].split("-");
-//                System.out.println("hiddens:" + Arrays.toString(hidden));
                 String[] given_split = probs[0].split("\\|");
-//                System.out.println(Arrays.toString(given_split));
                 String query = given_split[0];
-//                System.out.println("q:" + query);
                 String[] evi = given_split[1].split(",");
-//                System.out.println("evi:" + Arrays.toString(evi));
                 variableElimination ve = new variableElimination(query, hidden, evi, BN);
                 ans += ve.variableElimination() + "\n";
             }
             else {
                 String[] given_split = lines.get(i).split("\\|");
                 String[] query = given_split[0].split("-");
-//                System.out.println(Arrays.toString(given_split));
                 ArrayList<bayesianNode> evidence = new ArrayList<>();
                 if (given_split.length > 1) {
                     String[] ev = given_split[1].split(",");
-//                System.out.println(Arrays.toString(ev));
                     for (int j = 0; j < ev.length; j++) {
                         String[] one = ev[j].split("=");
-//                        System.out.println(Arrays.toString(one));
                         bayesianNode e = BN.returnByName(one[0]);
-//                        System.out.println((e));
                         evidence.add(e);
                     }
                 }
-//                System.out.println(evidence);
                 bayesianNode src = BN.returnByName(query[0]);
                 bayesianNode dest = BN.returnByName(query[1]);
 
